5afed09f71743d48fc67ba9e1e1f330b2426ebe0
Marshal Amos <amosmarshal@gmail.com>
2022-10-18 02:08:21 -0700

    Creating a file: _printf.c that produces output according to a format

---
+++ _printf.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _printf - a function that produces output according to a format
+ *
+ * @format: a character string.
+ *
+ * Return: the number of characters printed excluding the terminating null byte
+ */
+
+int _printf(const char *format, ...)
+{
+	int printed_chars;
+
+	/* a structure that list the specifiers */
+	convert_t specifier_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+
+	va_list argument_list;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	va_start(argument_list);
+
+	/* calling the parsed function */
+	printed_chars = parsed_func(format, specifier_list, argument_list);
+	va_end(argument_list);
+	return (printed_chars);
+}
